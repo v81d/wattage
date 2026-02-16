d297b843a38c88d0f978914c7ffe2da65cd288d6
v81d <v81d@proton.me>
2026-02-16 12:06:37 -0500

    build(nix-shell): simplify function exports

--- shell.nix
+++ shell.nix
@@ -4,6 +4,8 @@
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
+    vala-language-server
+    uncrustify
     meson
     ninja
     vala
@@ -29,18 +31,17 @@ pkgs.mkShell {
       meson setup build --reconfigure
       meson compile -C build
     }
-    export -f build
 
     install() {
       DESTDIR=staging meson install -C build
       glib-compile-schemas build/staging/usr/local/share/glib-2.0/schemas
     }
-    export -f install
 
     run() {
       GSETTINGS_SCHEMA_DIR=build/staging/usr/local/share/glib-2.0/schemas ./build/staging/usr/local/bin/wattage
     }
-    export -f run
+
+    export -f build install run
 
     echo -e "  \033[1;33mbuild\033[0m     Build the project in the build/ directory"
     echo -e "  \033[1;33minstall\033[0m   Install the project to the build/staging/ directory"
