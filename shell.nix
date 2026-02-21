6c0b33b2f12109e7db40f21cc53aed54ee8b0783
v81d <v81d@proton.me>
2026-02-21 12:42:17 -0500

    build(nix-shell): add translation management functions

--- shell.nix
+++ shell.nix
@@ -1,7 +1,4 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     vala-language-server
@@ -41,7 +38,23 @@ pkgs.mkShell {
       GSETTINGS_SCHEMA_DIR=build/staging/usr/local/share/glib-2.0/schemas ./build/staging/usr/local/bin/wattage
     }
 
-    export -f build install run
+    tupdate() {
+      xgettext -f po/POTFILES.in -o po/wattage.pot --package-name=Wattage --keyword=_
+    }
+
+    tmerge() {
+      for pofile in po/*.po; do
+        msgmerge --update "$pofile" po/wattage.pot
+      done
+    }
+
+    tcleanup() {
+      for f in po/*.po; do
+        msgattrib --no-obsolete -o "$f" "$f"
+      done
+    }
+
+    export -f build install run tupdate tmerge tcleanup
 
     echo -e "  \033[1;33mbuild\033[0m     Build the project in the build/ directory"
     echo -e "  \033[1;33minstall\033[0m   Install the project to the build/staging/ directory"
